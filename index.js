8dc81690f599e86dcd3a2b19e3f989fe51ca1e50
mrdiextra <tid190032@gmail.com>
2026-03-29 16:18:22 +0530

    Revert "preshn "
    This reverts commit f7fd5748edaa437a51d573bd1627974d099213a7.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-import express from "express";
+const express = require("express");
+const {userModel} = require("./db");
+const app = express();
+const port = 3000;
+
+
+
+app.use("/user/api",userModel);
+// Middleware
+app.use(express.json());
+
+// User signup
+app.get("/", function (req, res) {
+    return res.json({
+        message: "User home up successfully"
+    });
+});
+app.post("/user/signup", function (req, res) {
+    return res.json({
+        message: "User signed up successfully"
+    });
+});
+
+// User signin
+app.post("/user/signin", function (req, res) {
+    return res.json({
+        message: "User signed in successfully"
+    });
+});
+
+// Get user purchases
+app.get("/user/purchases", function (req, res) {
+    return res.json({
+        message: "User purchases fetched successfully"
+    });
+});
+
+// Purchase a course
+app.post("/course/purchase", function (req, res) {
+    return res.json({
+        message: "Course purchased successfully"
+    });
+});
+
+// Get all courses
+app.get("/courses", function (req, res) {
+    return res.json({
+        message: "Courses fetched successfully"
+    });
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
