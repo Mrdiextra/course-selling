0856bd05b4df9229cc520180f6e0489883e3cfe8
mrdiextra <tid190032@gmail.com>
2026-03-29 16:14:14 +0530

    Revert "hello"
    This reverts commit f7fd5748edaa437a51d573bd1627974d099213a7.

--- index.js
+++ index.js
@@ -1,56 +1,36 @@
+// import express from "express";
 const express = require("express");
-const {userModel} = require("./db");
+
 const app = express();
 const port = 3000;
 
-hello.js
 
 
-app.use("/user/api",userModel);
-// Middleware
-app.use(express.json());
-
-// User signup
-app.get("/", function (req, res) {
-    return res.json({
-        message: "User home up successfully"
-    });
-});
-app.post("/user/signup", function (req, res) {
-    return res.json({
-        message: "User signed up successfully"
+app.post("/user/signup", function( req, res) {
+    res.json({
+        message  :"signup endpoint"
     });
 });
 
-// User signin
-app.post("/user/signin", function (req, res) {
-    return res.json({
-        message: "User signed in successfully"
+app.post("/user/signin", function(req ,res) {
+    res.json({
+        message : "this signin endpoint"
     });
 });
 
-// Get user purchases
-app.get("/user/purchases", function (req, res) {
-    return res.json({
-        message: "User purchases fetched successfully"
+app.post("/user/perchese", function(req ,res) {
+    res.json({
+        message : "this the perchse endpoint"
     });
 });
 
-// Purchase a course
-app.post("/course/purchase", function (req, res) {
-    return res.json({
-        message: "Course purchased successfully"
+
+app.get("/user/courses",function(req,res) {
+    res.json({
+        message  : "this the couse end point"
     });
 });
 
-// Get all courses
-app.get("/courses", function (req, res) {
-    return res.json({
-        message: "Courses fetched successfully"
-    });
-});
-
-// Start server
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
